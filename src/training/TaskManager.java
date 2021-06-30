2db849193de4b4867c78845db8b2ea37e957b722
Michał Kępiński <kepinski.poznan@gmail.com>
2021-06-30 20:25:19 +0200

    Added help section, new task adding, task display and partially task removal. Left to do - task removal, console colors, simple security.

--- src/training/TaskManager.java
+++ src/training/TaskManager.java
@@ -1,9 +1,11 @@
 package training;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
 import training.ConsoleColors;
@@ -11,22 +13,25 @@ import training.ConsoleColors;
 public class TaskManager {
 	
 	private static String welcomeMsg = "Welcome to TaskManager!";
+	private static String ioExcMsg = "There is a problem with reading from/writing the file!";
+	private static String fileNotFoundExcMsg = "File could not be found!";
+	private static String fileAbsolutePath = "C:\\Users\\Kempinov\\git\\Tasks\\src\\training\\tasks.csv";
 	
 	public static void main(String[] args) {
 		
 		System.out.println(welcomeMsg);
 		String input = null;
 		Scanner userInputScanner = new Scanner(System.in);
-		Path file = Paths.get("C:\\Users\\Kempinov\\git\\Tasks\\src\\training\\tasks.csv");
+		Path file = Paths.get(fileAbsolutePath);
 		Scanner fileScanner = null;
 		
 		try {
 			fileScanner = new Scanner(file);
 		} catch(FileNotFoundException fnfe) {
-			System.out.println("File could not be found!");
+			System.out.println(fileNotFoundExcMsg);
 		} catch(IOException ioe) {
 			ioe.getStackTrace();
-			System.out.println("There is a problem with reading from/writing the file!");
+			System.out.println(ioExcMsg);
 		}
 		
 		do {
@@ -37,6 +42,7 @@ public class TaskManager {
 		} while(Integer.valueOf(input) != 6);
 			
 		userInputScanner.close();
+		fileScanner.close();
 		quit();
 	}
 	
@@ -50,7 +56,19 @@ public class TaskManager {
 						   "6. Quit program\n");
 	}
 	
-	private static String[][] getFileContent(Scanner fileScanner) {
+	private static String[][] getFileContent() { 
+		Path file = Paths.get(fileAbsolutePath);
+		Scanner fileScanner = null;
+		
+		try {
+			fileScanner = new Scanner(file);
+		} catch(FileNotFoundException fnfe) {
+			System.out.println(fileNotFoundExcMsg);
+		} catch(IOException ioe) {
+			ioe.getStackTrace();
+			System.out.println(ioExcMsg);
+		}
+		
 		String[][] tasks = new String[0][3];
 		String line = null;
 		for(int i = 0; fileScanner.hasNextLine(); i++) {
@@ -76,14 +94,14 @@ public class TaskManager {
 	}
 	
 	private static String takeActionFromUser(Scanner userInputScanner) {
-		String response = userInputScanner.next();
+		String response = userInputScanner.nextLine();
 		return response;
 	}
 	
 	private static void processMenuSelection(String input, Scanner fileScanner, Scanner userInputScanner) {
 		switch (Integer.valueOf(input)) {
 		    case 1:
-		        addTask(userInputScanner);
+		        addTask(userInputScanner, fileScanner);
 		        break;
 		    case 2:
 		        updateTask();
@@ -92,7 +110,7 @@ public class TaskManager {
 		        listAllTasks(fileScanner);
 		        break;
 		    case 4:
-		        removeTask();
+		        removeTask(fileScanner, userInputScanner);
 		        break;
 		    case 5:
 		        help();
@@ -104,18 +122,34 @@ public class TaskManager {
 		}
 	}
 	
-	private static void displayTasks(Scanner fileScanner) {
-		String[][] tasks = getFileContent(fileScanner);
+	private static void displayTasks(Scanner fileScanner, boolean linesNumerated) {
+		
+		System.out.println();
+		System.out.println("- - -");
+		System.out.println();
+		
+		String[][] tasks = getFileContent();
+		int number = 1;
 		for(int i = 0; i < tasks.length; i++) {
 			String line = "";
 			for(int j = 0; j < tasks[i].length; j++) {
 				line += tasks[i][j];
 				if(j < 2) {
-					line += ", ";
+					line += "  ";
 				}
 			}
-			System.out.println(line);
+			
+			if(linesNumerated) {
+				System.out.printf("%s  %s\n",number++ , line);
+			} else {
+				System.out.println(line);
+			}
 		}
+		
+		System.out.println();
+		System.out.println("- - -");
+		System.out.println();
+		
 	}
 	
 	private static void quit() {
@@ -123,18 +157,22 @@ public class TaskManager {
 		System.out.println("Goodbye!");
 	}
 	
-	private static void addTask(Scanner userInputScanner) {
+	private static void addTask(Scanner userInputScanner, Scanner fileScanner) {
 		System.out.println("Please add task description:");
 		String description = takeActionFromUser(userInputScanner);
 		
-		System.out.println("Please add task due date: [YYYY-MM-DD]");
+		System.out.println("Please add task due date: [YYYY-MM-DD] or type in [today]");
 		String date = takeActionFromUser(userInputScanner);
+		if(date.equalsIgnoreCase("today")) {
+//			LocalDate now = new LocalDate();
+//			date = LocalDate.now();
+		}
 		
 		String importance = null;
 		do {
 			System.out.println("Is your task important? [yes/no]");
 			importance = takeActionFromUser(userInputScanner).toLowerCase();
-		} while(importance != "yes" || importance != "no");
+		} while(importance == "yes" || importance == "no");
 		
 		switch(importance) {
 			case "yes":
@@ -145,7 +183,28 @@ public class TaskManager {
 				break;
 		}
 		
-		System.out.printf("%s, %s, %s", description, date, importance);
+		String lineToBeSavedToFile = description + ", " + date + ", " + importance + "\n";
+		
+		String confirmation = null;
+		do {
+			System.out.println("This task will be saved: \n" + lineToBeSavedToFile);
+			System.out.println("Are you sure? [yes/no]");
+			confirmation = takeActionFromUser(userInputScanner).toLowerCase();
+		} while(confirmation == "yes" || confirmation == "no");
+		
+		if(confirmation.equalsIgnoreCase("yes")) {
+			
+			try {
+				FileWriter fw = new FileWriter(fileAbsolutePath, true);
+				fw.append(lineToBeSavedToFile);
+				fw.close();
+			} catch(IOException ioe) {
+				System.out.println(ioExcMsg);
+				ioe.getStackTrace();
+			}
+			System.out.println("Task has been successfully added!");
+		}
+		
 	}
 	
 	private static void updateTask() {
@@ -153,14 +212,17 @@ public class TaskManager {
 	}
 	
 	private static void listAllTasks(Scanner fileScanner) {
-		displayTasks(fileScanner);
+		displayTasks(fileScanner, false);
 	}
 	
-	private static void removeTask() {
-		System.out.println("remove");
+	private static void removeTask(Scanner fileScanner, Scanner userInputScanner) {
+		displayTasks(fileScanner, true);
+		System.out.println("Which task you wish to be removed? Type in the number on the left.");
+		String taskNumber = takeActionFromUser(userInputScanner);
 	}
 	
 	private static void help() {
-		System.out.println("help");
+		System.out.println("*** HELP SECTION ***");
+		System.out.println("1. To execute a command either type in the command's number(if given) or\n whatever the program asks you to. Next you should press ENTER.");
 	}
 }
