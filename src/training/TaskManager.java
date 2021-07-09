f9f94274d76653441a3660b5c8d27c3cb062ff2f
Michał Kępiński <kepinski.poznan@gmail.com>
2021-07-09 19:58:59 +0200

    Program works as intended, no bugs to be seen!

--- src/training/TaskManager.java
+++ src/training/TaskManager.java
@@ -41,7 +41,7 @@ public class TaskManager {
 		do {
 			
 				showMenu();
-				input = takeActionFromUser(userInputScanner);
+				input = takeLineFromUser(userInputScanner);
 				processMenuSelection(input, fileScanner, userInputScanner);	
 				
 		} while(!(input == null) && (!input.equals("6")));
@@ -61,7 +61,6 @@ public class TaskManager {
 						   "6. Quit program\n");
 	}
 
-	
 	private static List<String[]> getFileContent() { 
 		Path file = Paths.get(fileAbsolutePath);
 		Scanner fileScanner = null;
@@ -85,34 +84,31 @@ public class TaskManager {
 		return tasks;
 	}
 	
-	private static String takeActionFromUser(Scanner userInputScanner) {
-		String response = userInputScanner.nextLine();
-		return response;
+	private static String takeLineFromUser(Scanner userInputScanner) {
+		return userInputScanner.nextLine();
 	}
 	
 	private static void processMenuSelection(String input, Scanner fileScanner, Scanner userInputScanner) {
-		if(!input.isBlank()) {
-			switch (input) {
-			    case "1":
-			        addTask(userInputScanner, fileScanner);
-			        break;
-			    case "2":
-			        updateTask(fileScanner, userInputScanner);
-			        break;
-			    case "3":
-			        displayTasks(fileScanner, false);
-			        break;
-			    case "4":
-			        deleteTask(fileScanner, userInputScanner);
-			        break;
-			    case "5":
-			        help();
-			        break;
-			    case "6":
-			    	break;
-			    default:
-			        System.out.println("Please select a correct option.");
-			}
+		switch (input) {
+		    case "1":
+		        addTask(userInputScanner, fileScanner);
+		        break;
+		    case "2":
+		        updateTask(fileScanner, userInputScanner);
+		        break;
+		    case "3":
+		        displayTasks(fileScanner, false);
+		        break;
+		    case "4":
+		        deleteTask(fileScanner, userInputScanner);
+		        break;
+		    case "5":
+		        help();
+		        break;
+		    case "6":
+		    	break;
+		    default:
+		        System.out.println("Please select a correct option.");
 		}
 	}
 	
@@ -129,7 +125,7 @@ public class TaskManager {
 			if(linesNumerated) {
 				System.out.println(String.format("%s   %s - %s - %s", number++, tasks.get(i)[0].trim(), tasks.get(i)[1].trim(), tasks.get(i)[2].trim()));
 			} else {
-				System.out.println(String.format("%s, %s, %s", tasks.get(i)[0].trim(), tasks.get(i)[1].trim(), tasks.get(i)[2].trim()));
+				System.out.println(String.format("%s - %s - %s", tasks.get(i)[0].trim(), tasks.get(i)[1].trim(), tasks.get(i)[2].trim()));
 			}
 		}
 		
@@ -139,32 +135,9 @@ public class TaskManager {
 		
 	}
 	
-	public static boolean dateValid(String dateStr) {
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            LocalDate.parse(dateStr, formatter);
-        } catch (DateTimeParseException e) {
-            return false;
-        }
-        return true;
-    }
-	
 	private static void quit() {
 		System.out.println("Thank you for using TaskManager.");
 		System.out.println("Goodbye!");
-	}
-	
-	private static boolean checkTaskNumber(int input) {
-		return (input >= 1 && input <= getFileContent().size());
-	}
-	
-	private static boolean checkDate(String date) {
-		if(date.equalsIgnoreCase("today")) {
-			return true;
-		} else if(dateValid(date)) {
-			return true;
-		}
-		return false;
 	}
 	
 	private static void addTask(Scanner userInputScanner, Scanner fileScanner) {
@@ -172,24 +145,23 @@ public class TaskManager {
 		String description = null;
 		do {
 			System.out.println("Please add task description: [max 40 signs]");
-			description = takeActionFromUser(userInputScanner);
+			description = takeLineFromUser(userInputScanner);
 		} while(description.length() >= 40 || description.length() <= 1);
 		
 		String date = null;
 		do {
 			System.out.println("Please add task due date: [YYYY-MM-DD] or type in [today]");
-			date = takeActionFromUser(userInputScanner);
+			date = takeLineFromUser(userInputScanner);
 		} while(!checkDate(date));
 		
 		if(date.equalsIgnoreCase("today")) {
-			LocalDate today = LocalDate.now();
-			date = today.toString();
+			date = LocalDate.now().toString();
 		}
 		
 		String importance = null;
 		do {
 			System.out.println("Is your task important? [yes/no]");
-			importance = takeActionFromUser(userInputScanner).toLowerCase();
+			importance = takeLineFromUser(userInputScanner).toLowerCase();
 		} while(!importance.equals("yes") && !importance.equals("no"));
 		
 		switch(importance) {
@@ -201,28 +173,27 @@ public class TaskManager {
 				break;
 		}
 
-		String lineToBeSavedToFile = description + ", " + date + ", " + importance + "\n";
+		String lineToBeAddedToFile = description + ", " + date + ", " + importance + "\n";
 		
 		String confirmation = null;
 		do {
-			System.out.println("This task will be saved: \n" + lineToBeSavedToFile);
-			System.out.println("Are you sure? [yes/no]");
-			confirmation = takeActionFromUser(userInputScanner).toLowerCase();
+			System.out.println(ConsoleColors.GREEN + "This task will be saved: \n" + lineToBeAddedToFile);
+			System.out.println(ConsoleColors.RED + "Are you sure? [yes/no]");
+			confirmation = takeLineFromUser(userInputScanner).toLowerCase();
 		} while(!confirmation.equals("yes") && !confirmation.equals("no"));
 		
 		if(confirmation.equalsIgnoreCase("yes")) {
 			
 			try {
 				FileWriter fw = new FileWriter(fileAbsolutePath, true);
-				fw.append(lineToBeSavedToFile);
+				fw.append(lineToBeAddedToFile);
 				fw.close();
+				System.out.println(ConsoleColors.GREEN + "Task has been successfully added!" + ConsoleColors.RESET);
 			} catch(IOException ioe) {
 				System.out.println(ioExcMsg);
 				ioe.getStackTrace();
 			}
-			System.out.println("Task has been successfully added!");
 		}
-		
 	}
 	
 	private static void updateTask(Scanner fileScanner, Scanner userInputScanner) {
@@ -233,26 +204,28 @@ public class TaskManager {
 		String string_input = null;
 		while(whileBoolVar) {
 			System.out.println("Which task you wish to change? Type in its number.");
-			string_input = takeActionFromUser(userInputScanner);
+			string_input = takeLineFromUser(userInputScanner);
 			if(checkIfInputIsNumber(string_input) && checkTaskNumber(Integer.valueOf(string_input))) {
 				whileBoolVar = false;
+			} else {
+				System.out.println("Incorrect value!");
 			}
 		}
 		
 		int int_input = Integer.valueOf(string_input);
 		
-		System.out.printf("This task will be changed: %s - %s - %s\n", tasks.get(int_input)[0], tasks.get(int_input)[1], tasks.get(int_input)[2]);
+		System.out.printf(ConsoleColors.GREEN + "This task will be changed: %s - %s - %s\n", tasks.get(int_input)[0], tasks.get(int_input)[1], tasks.get(int_input)[2] + ConsoleColors.RESET);
 
 		String description = null;
 		do {
 			System.out.println("New task description:: [max 40 signs]");
-			description = takeActionFromUser(userInputScanner);
+			description = takeLineFromUser(userInputScanner);
 		} while(description.length() >= 40 || description.length() <= 1);
 		
 		String date = null;
 		do {
 			System.out.println("Please add task due date: [YYYY-MM-DD] or type in [today]");
-			date = takeActionFromUser(userInputScanner);
+			date = takeLineFromUser(userInputScanner);
 		} while(!checkDate(date));
 		
 		if(date.equalsIgnoreCase("today")) {
@@ -263,7 +236,7 @@ public class TaskManager {
 		String importance = null;
 		do {
 			System.out.println("Is your task important? [yes/no]");
-			importance = takeActionFromUser(userInputScanner).toLowerCase();
+			importance = takeLineFromUser(userInputScanner).toLowerCase();
 		} while(!importance.equals("yes") && !importance.equals("no"));
 		
 		switch(importance) {
@@ -277,8 +250,8 @@ public class TaskManager {
 		
 		String confirmation = null;
 		do {
-			System.out.println("Are you sure? [yes/no]");
-			confirmation = takeActionFromUser(userInputScanner).toLowerCase();
+			System.out.println(ConsoleColors.RED + "Are you sure? [yes/no]");
+			confirmation = takeLineFromUser(userInputScanner).toLowerCase();
 		} while(!confirmation.equals("yes") && !confirmation.equals("no"));
 		
 		if(confirmation.equalsIgnoreCase("yes")) {
@@ -288,7 +261,7 @@ public class TaskManager {
 			tasks.get(int_input)[2] = importance;
 			
 			writeToFile(tasks);
-			System.out.println("Task has been successfully updated!");
+			System.out.println(ConsoleColors.GREEN + "Task has been successfully updated!" + ConsoleColors.RESET);
 		}
 	}
 
@@ -305,70 +278,67 @@ public class TaskManager {
 			ioe.getStackTrace();
 		}
 	}
+
+	private static void deleteTask(Scanner fileScanner, Scanner userInputScanner) {
+		displayTasks(fileScanner, true);
+		boolean whileBoolVar = true;
+		String string_input = null;
+		while(whileBoolVar) {
+			System.out.println("Which task you wish to delete? Type in its number.");
+			string_input = takeLineFromUser(userInputScanner);
+			if(checkIfInputIsNumber(string_input) && checkTaskNumber(Integer.valueOf(string_input))) {
+				whileBoolVar = false;
+			}
+		}
+		List<String[]> tasks = getFileContent();
+		
+		String confirmation = null;
+		do {
+			System.out.println(ConsoleColors.RED + "Are you sure? [yes/no]");
+			confirmation = takeLineFromUser(userInputScanner).toLowerCase();
+		} while(!confirmation.equals("yes") && !confirmation.equals("no"));
+		
+		if(confirmation.equalsIgnoreCase("yes")) {
+			writeToFile(tasks);
+			System.out.println(ConsoleColors.GREEN + "Task has been successfully deleted!" + ConsoleColors.RESET);
+		}
+	}
+	
+	private static void help() {
+
+
+		
+
+		System.out.println("*** HELP SECTION ***");
+		System.out.println("1. To execute a command either type in the command's number(if given) or\n whatever the program asks you to. Next you should press ENTER.\n");
+	}
 	
 	private static boolean checkIfInputIsNumber(String input) {
 		try {
 			Integer.valueOf(input);
-		} catch(NumberFormatException nfe) {
+		} catch (NumberFormatException nfe) {
+			System.out.println("Incorrect value!");
 			return false;
 		}
 		return true;
 	}
 	
-	private static void deleteTask(Scanner fileScanner, Scanner userInputScanner) {
-//		displayTasks(fileScanner, true);
-//		boolean whileBoolVar = true;
-//		String string_input = null;
-//		while(whileBoolVar) {
-//			System.out.println("Which task you wish to delete? Type in its number.");
-//			string_input = takeActionFromUser(userInputScanner);
-//			if(Character.isDigit(string_input.charAt(0))) {
-//				whileBoolVar = false;
-//			}
-//		}
-//		int int_input = Integer.valueOf(string_input) - 1;
-//		
-//		String[][] tasksArray = getFileContent();
-//		List<String> tasksList = new ArrayList<>();
-//		for(int i = 0; i < tasksArray.length; i++) {
-//			String line = "";
-//			for(int j = 0; j < tasksArray[i].length; j++) {
-//				if(i != int_input) {
-//					line += tasksArray[i][j];
-//					if(j < 2) {
-//						line += ", ";
-//					}
-//				}
-//			}
-//			tasksList.add(line);
-//		}
-//		
-//		String confirmation = null;
-//		do {
-//			System.out.println("Are you sure? [yes/no]");
-//			confirmation = takeActionFromUser(userInputScanner).toLowerCase();
-//		} while(!confirmation.equals("yes") && !confirmation.equals("no"));
-//		
-//		if(confirmation.equalsIgnoreCase("yes")) {
-//			
-//			try {
-//
-//				FileWriter fw = new FileWriter(fileAbsolutePath, false);
-//				for (String s : tasksList) {
-//					fw.append(s.trim()).append("\n");
-//				}
-//				
-//				fw.close();
-//			} catch(IOException ioe) {
-//				System.out.println(ioExcMsg);
-//				ioe.getStackTrace();
-//			}
-//			System.out.println("Task has been successfully deleted!");
-//		}
+	private static boolean checkTaskNumber(int input) {
+		return (input >= 1 && input <= getFileContent().size());
 	}
 	
-	private static void help() {
-		System.out.println("*** HELP SECTION ***");
-		System.out.println("1. To execute a command either type in the command's number(if given) or\n whatever the program asks you to. Next you should press ENTER.\n");
-	}
+	public static boolean checkDate(String dateStr) {
+		if(dateStr.equals("today")) {
+			return true;
+		} else {
+	        try {
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	            LocalDate.parse(dateStr, formatter);
+	        } catch (DateTimeParseException e) {
+	        	System.out.println(ConsoleColors.RED + "Date is incorrect!" + ConsoleColors.RESET);
+	            return false;
+	        }
+		}
+        return true;
+    }
 }
